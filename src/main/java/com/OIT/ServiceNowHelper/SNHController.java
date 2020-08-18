1a19caee6ff195fbb4be8bd846a3a2503f1fd906
utkarsh06 <utkarsh.gaur06@gmail.com>
2020-08-18 19:32:50 -0400

    added controller and helper html page

---
+++ src/main/java/com/OIT/ServiceNowHelper/SNHController.java
@@ -0,0 +1,23 @@
+package com.OIT.ServiceNowHelper;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+	
+@Controller
+public class SNHController {
+
+	@GetMapping(path = "/helper")
+	public String help(Model model) {
+		System.out.println("this is done");
+		return "helper";
+	}
+	
+	
+	@PostMapping(path = "/helper")
+	public String helped(){
+		return "helper";
+	}
+
+}
