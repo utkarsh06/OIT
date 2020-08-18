25a88bc57352c462f9c202eb7f03629401639c31
utkarsh06 <utkarsh.gaur06@gmail.com>
2020-08-17 23:08:29 -0400

    Initial commit

---
+++ src/main/java/com/OIT/ServiceNowHelper/ServiceNowHelperApplication.java
@@ -0,0 +1,13 @@
+package com.OIT.ServiceNowHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceNowHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceNowHelperApplication.class, args);
+	}
+
+}
